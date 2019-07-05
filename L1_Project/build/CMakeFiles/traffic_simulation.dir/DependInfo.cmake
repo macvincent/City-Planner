0addf0b8e8c3a9757c6f7dc9c6a2c7230d530178
macvincentaghaoko@gmail.com <macvincentaghaoko@gmail.com>
2019-07-05 15:00:56 -0400

    src/TrafficObject: join all traffic object threads

---
+++ L1_Project/build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/Graphics.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/Intersection.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/Street.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/TrafficObject.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/TrafficSimulator-L1.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L1.cpp.o"
+  "/home/macvincent/Desktop/city-planner/L1_Project/src/Vehicle_Student.cpp" "/home/macvincent/Desktop/city-planner/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle_Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ L1_Project/build/CMakeFiles/traffic_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L1.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/src/Vehicle_Student.cpp.o"
+  "traffic_simulation"
+  "traffic_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traffic_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
